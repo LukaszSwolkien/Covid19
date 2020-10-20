2c27e67317711388f870e84afd7242abc7c9c0b7
lukasz <lukasz.swolkien@gmail.com>
2020-10-20 12:14:21 +0200

    feat: added world map with stats for countries we have data for

--- World/figures.py
+++ World/figures.py
@@ -42,6 +42,49 @@ def case_distribution(data: list, title: str) -> go.Figure:
     return fig
 
 
+@helpers.trace_function("Data timeline multi-graph")
+def distribution_subplots(data: list, selected_metrics: list, country_labels: list, title: str) -> go.Figure:
+
+    assert len(data) == len(country_labels)
+    assert len(selected_metrics) > 0
+    N = len(country_labels)
+
+    fig = make_subplots(
+        rows=len(country_labels),
+        cols=1,
+        subplot_titles=country_labels,
+        shared_xaxes=True,
+        vertical_spacing=0.05,
+    )
+
+    date_rep_counter = {}
+    for d in data:
+        for i in d:
+            date_rep_counter[i["dateRep"]] = date_rep_counter.get(i["dateRep"], 0) + 1
+
+    date_rep_common = [k for k, v in date_rep_counter.items() if v == N]
+
+    for n in range(0, N):
+
+        for s in selected_metrics:
+            fig.add_trace(
+                go.Scatter(
+                    x=[i["dateRep"] for i in data[n] if i["dateRep"] in date_rep_common],
+                    y=[
+                        i[s]
+                        for i in data[n]
+                        if i["dateRep"] in date_rep_common
+                    ],
+                    mode=MODE_LINES_N_MARKERS,
+                    name=f"{country_labels[n]} {s.replace('_',' ')}",
+                ),
+                row=n + 1,
+                col=1,
+            )
+    fig.update_layout(height=1200, width=1000, title_text=title)
+    return fig
+
+
 @helpers.trace_function("Case distribution multi-graph")
 def case_distribution_subplots(data: list, country_labels: list, title: str) -> go.Figure:
 
@@ -90,7 +133,7 @@ def case_distribution_subplots(data: list, country_labels: list, title: str) -> 
             row=n + 1,
             col=1,
         )
-
+        
         fig.add_trace(
             go.Bar(
                 x=[i["dateRep"] for i in data[n] if i["dateRep"] in dateRep_common],
@@ -100,5 +143,6 @@ def case_distribution_subplots(data: list, country_labels: list, title: str) -> 
             row=n + 1,
             col=1,
         )
+
     fig.update_layout(height=1200, width=1000, title_text=title)
     return fig
--- run.py
+++ run.py
@@ -18,13 +18,17 @@ tracer = trace.get_tracer(__name__)
 
 import World.ecdc as ecdc
 import plotly.graph_objects as go
+import World.countries as countries
+import Poland.poland_stats as pl
 
-with tracer.start_as_current_span(f"Poland weekly"):
-    cases = ecdc.cases_by_country()
-    testing = ecdc.testing_by_country_weekly()
-    hospital_rates = ecdc.hospitel_admission_rates_weekly()
+population = countries.load_countries_data()
 
-    data = ecdc.weekly(cases, "Poland", testing, hospital_rates)
+# with tracer.start_as_current_span(f"Poland weekly"):
+cases = ecdc.cases_by_country()
+testing = ecdc.testing_by_country_weekly()
+hospital_rates = ecdc.hospitel_admission_rates_weekly()
+
+data = ecdc.weekly(cases, "Poland", testing, hospital_rates)
 
 fig = go.Figure()
 
@@ -47,3 +51,46 @@ fig.add_trace(
     go.Bar(x=[i["dateRep"] for i in data], y=[i["cases"] for i in data], name="cases")
 )
 fig.show()
+
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+
+df_pl = pl.covid_19_timeline()
+
+summary = df_pl.aggregate({pl.DEATHS_DAILY:['sum', 'max'], 
+              pl.CONFIRMED_DAILY:['sum', 'max'],
+              }) 
+
+summary['confirmed ratio'] = summary[pl.CONFIRMED_DAILY]['sum']*100/population['Poland']
+summary['country'] = 'Poland'
+summary['active'] = df_pl[pl.ACTIVE][-1]
+summary['active ratio'] = summary.active*100/population['Poland']
+layout = dict(title = 'Covid19 ratio',
+              geo = dict(projection = {'type':'mercator'}),
+              height=800,
+              margin={"r":0,"t":30,"l":0,"b":0}
+             )
+
+data = dict(
+        
+        type = 'choropleth',
+        colorscale = 'Viridis',
+        locations = summary['country'],
+        locationmode = "country names",
+        z = summary['confirmed ratio'],
+        text = summary['country'],
+        colorbar = {'title' : 'Ratio'},
+      )
+
+
+
+choromap = go.Figure(data = [data],layout = layout)
+
+choromap.update_geos(
+    visible=False, scope="europe",
+    showcountries=True, countrycolor="Black",
+    showsubunits=True, subunitcolor="Blue"
+)
+choromap.show()
