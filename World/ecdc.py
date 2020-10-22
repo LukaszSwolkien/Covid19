b1632c8e054978981f04cf65b6aed46892113e3f
lukasz <lukasz.swolkien@gmail.com>
2020-10-22 14:16:57 +0200

    simplified code using pandas

--- World/ecdc.py
+++ World/ecdc.py
@@ -5,6 +5,8 @@ from collections import Counter
 from common.helpers import to_date, week_number, trace_function
 from World.corrections import CORRECTION_POLAND_05_10, CORRECTION_POLAND_06_10
 from typing import Union
+import pandas as pd
+from functools import reduce
 
 DATE_REP = "dateRep"
 YEAR = "year"
@@ -14,164 +16,71 @@ CASES = "cases"
 DEATHS = "deaths"
 TESTS_DONE = "tests_done"
 HOSPITAL_RATE = "hospital_rate"
-NEW_CASES = "new_cases"
 TESTS_DONE = "tests_done"
 TESTING_RATE = "testing_rate"
-POSITIVITY_RATE = "positivity_rate"
+POSITIVITY_RATE = "testing_positivity_rate"
 TESTING_DATA_SOURCE = "testing_data_source"
 COUNTRY = 'country'
+YEAR_WEEK = 'year_week'
+
+
+def __norm_year_week(yw):
+    yw = yw.split('W')
+    year = int(yw[0][0:4])
+    week = yw[1]
+    return f'{year}-W{int(week)}' # remove leading zeros
 
 
 @trace_function("Get cases")
 def cases_by_country() -> list:
     cases = requests.get(url=settings.ECDC_CASE_DISTRIBUTION_URL)
-    return cases.json().get("records", [])
+    df = pd.DataFrame(cases.json().get("records", [])).rename(columns={"countriesAndTerritories": COUNTRY})
+    return df
 
 
 @trace_function("Get testing")
 def testing_by_country_weekly() -> list:
     testing = requests.get(url=settings.ECDC_COVID19_TESTING_URL)
-    return testing.json()
+    df = pd.DataFrame(testing.json()).rename(columns={"new_cases": CASES}).reset_index()
+    df[YEAR_WEEK] = df[YEAR_WEEK].apply(__norm_year_week)
+    return df
 
 
 @trace_function("Get hospital rates")
 def hospitel_admission_rates_weekly() -> list:
     admission_rates = requests.get(url=settings.ECDC_HOSPITAL_ADMISSION_RATES_URL)
-    return admission_rates.json()
-
-
-def __select_and_sort(data, country):
-    selector = lambda x: (x["countriesAndTerritories"] == country)
-    selected_data = list(filter(selector, data))
-    return sorted(
-        selected_data, key=lambda x: (int(x[YEAR]), int(x[MONTH]), int(x[DAY]))
-    )
-
-
-def __aggregate(f, data):
-    agg_data = dict()
-    z_stats = {CASES: 0, DEATHS: 0}
-    for v in data:
-        ym = f(v)
-        curr = agg_data.get(ym, z_stats)
-        summary = Counter(curr) + Counter({CASES: v["cases"], DEATHS: v["deaths"]})
-        agg_data[ym] = summary
-    return agg_data
-
-def __timeline_dict(data):
-    res = {}
-
-    for d in data:
-        e = [{
-                DATE_REP: k,
-                CASES: v.get("cases", 0),
-                DEATHS: v.get("deaths", 0),
-                TESTS_DONE: v.get("testing", {}).get("tests_done", None),
-                HOSPITAL_RATE: v.get("hospital", {}).get("value", None),
-            } for k, v in data[d].items()]
-        res[d] = e
-    return res
-
-
-def __timeline_list(data):
-    res = []
-
-    for d in data:
-        e = [{
-                DATE_REP: k,
-                CASES: v.get("cases", 0),
-                COUNTRY: d,
-                DEATHS: v.get("deaths", 0),
-                TESTS_DONE: v.get("testing", {}).get("tests_done", None),
-                HOSPITAL_RATE: v.get("hospital", {}).get("value", None),
-            } for k, v in data[d].items()]
-        res += e
-    return res
-
-
-@trace_function("Curate data for daily load")
-def daily(cases: list, country: str) -> list:
-    sorted_selected_data = __select_and_sort(cases, country)
-
-    def correct_fun(x):
-        if country != "Poland": # we know data correction only for Poland, but this function may need to be extended to other countries too.
-            return x
-        if x[DATE_REP] == "05/10/2020":
-            return CORRECTION_POLAND_05_10
-        elif x[DATE_REP] == "06/10/2020":
-            return CORRECTION_POLAND_06_10
-        return x
-
-    return list(map(correct_fun, sorted_selected_data))
-
-# TODO - refactor to pd.DataFrame and simplify this code.
-@trace_function("Combine and aggregate weekly data")
-def weekly(
-    cases: list, country: str, testing: list = [], hospital_rates: list = []
-) -> list:
-    """Data aggregated weekly
-
-    """
-    sorted_selected_data = __select_and_sort(cases, country)
-    weekly_cases = {}
-    weekly_cases[country] = __aggregate(
-        lambda v: f'{v[YEAR]}-W{week_number(to_date(v))}', sorted_selected_data
-    )
-
-    for td in testing:
-        if td["country"] == country and td["year_week"] in weekly_cases:
-            weekly_cases[td["year_week"]]["testing"] = {
-                NEW_CASES: td.get("new_cases"),
-                TESTS_DONE: td.get("tests_done"),
-                TESTING_RATE: td.get("testing_rate"),
-                POSITIVITY_RATE: td.get("positivity_rate"),
-                TESTING_DATA_SOURCE: td.get("testing_data_source", ""),
+    df = pd.DataFrame(admission_rates.json()).rename(
+            columns={
+                'value':HOSPITAL_RATE,
+                'source': 'hospital_rate_source',
+                'url': 'hospital_rate_url'
             }
-    for hr in hospital_rates:
-        if hr["country"] == country and hr["year_week"] in weekly_cases:
-            weekly_cases[hr["year_week"]]["hospital"] = {
-                "value": hr.get("value"),
-                "source": hr.get("source", ""),
-                "url": hr.get("url", ""),
-            }
-
-    return __timeline_list(weekly_cases)
-
-# TODO - refactor to pd.DataFrame and simplify this code.
-@trace_function("Combine and aggregate weekly data")
-def weekly_all_countries(
-    cases: list, testing: list = [], hospital_rates: list = [], flatten=True,
-) -> Union[list,dict]:
-    """Data aggregated weekly
-
-    """
-    weekly_cases = {}
-    countries = list(set(dic['countriesAndTerritories'] for dic in cases)) 
-    for country in countries:
-        sorted_selected_data = __select_and_sort(cases, country)
-
-        weekly_cases[country] = __aggregate(
-            lambda v: f'{v[YEAR]}-W{week_number(to_date(v))}', sorted_selected_data
         )
-
-        for td in testing:
-            if td["country"] == country and td["year_week"] in weekly_cases[country].keys():
-                weekly_cases[country][td["year_week"]]["testing"] = {
-                    NEW_CASES: td.get("new_cases"),
-                    TESTS_DONE: td.get("tests_done"),
-                    TESTING_RATE: td.get("testing_rate"),
-                    POSITIVITY_RATE: td.get("positivity_rate"),
-                    TESTING_DATA_SOURCE: td.get("testing_data_source", ""),
-                }
-        for hr in hospital_rates:
-            if hr["country"] == country and hr["year_week"] in weekly_cases[country].keys():
-                weekly_cases[country][hr["year_week"]]["hospital"] = {
-                    "value": hr.get("value"),
-                    "source": hr.get("source", ""),
-                    "url": hr.get("url", ""),
-                }
-
-    return __timeline_list(weekly_cases) if flatten else __timeline_dict(weekly_cases)
+    df[YEAR_WEEK] = df[YEAR_WEEK].apply(__norm_year_week)
+    return df
 
 
+@trace_function("Combine data sets and aggregate weekly ")
+def weekly(
+    cases: pd.DataFrame, 
+    testing: pd.DataFrame = None, 
+    hospital_rates: pd.DataFrame = None
+) -> pd.DataFrame:
+    """Combine provided data sets and aggregate statistics weekly
+
+    """
+    cases[YEAR_WEEK] = cases.apply(lambda row: f"{row['year']}-W{week_number(to_date(row))}", axis=1)
+    df_final = cases.groupby([COUNTRY, YEAR_WEEK])[[CASES, DEATHS]].agg('sum').reset_index()
+    
+    if testing is not None:
+        testing_data = testing.set_index([COUNTRY, YEAR_WEEK])
+        cols_to_use = testing.columns.difference(df_final.columns)
+        df_final = pd.merge(df_final, testing_data[cols_to_use], on=[COUNTRY, YEAR_WEEK], how="outer").set_index([COUNTRY, YEAR_WEEK])
+    
+    if hospital_rates is not None:
+        hospital_data = hospital_rates.groupby([COUNTRY, YEAR_WEEK])[[HOSPITAL_RATE]].agg('sum') # some countries have daily data for hospital rate
+        cols_to_use = hospital_data.columns.difference(df_final.columns)
+        df_final = pd.merge(df_final, hospital_data[cols_to_use], on=[COUNTRY, YEAR_WEEK], how="outer")
+        
+    return df_final.reset_index()
 
--- World/figures.py
+++ World/figures.py
@@ -9,85 +9,6 @@ import numpy as np
 
 MODE_LINES_N_MARKERS = "lines+markers"
 
-# @helpers.trace_function("Case distribution graph")
-# def case_distribution(data: list, title: str) -> go.Figure:
-#     fig = go.Figure()
-
-#     try:
-#         fig.add_trace(
-#             go.Scatter(
-#                 x=[i["dateRep"] for i in data],
-#                 y=[i["hospital_rate"] for i in data],
-#                 mode=MODE_LINES_N_MARKERS,
-#                 name="hospital rate",
-#             )
-#         )
-#     except KeyError:
-#         print(f"Skipping hospital admittion rates due to missing data")
-
-#     fig.add_trace(
-#         go.Scatter(
-#             x=[i["dateRep"] for i in data], y=[i["deaths"] for i in data], name="deaths"
-#         )
-#     )
-
-#     fig.add_trace(
-#         go.Bar(
-#             x=[i["dateRep"] for i in data], y=[i["cases"] for i in data], name="cases"
-#         )
-#     )
-
-#     fig.update_layout(
-#         title=title,
-#         yaxis_title="daily increase",
-#         legend_title="Legend",
-#     )
-
-#     return fig
-
-
-# @helpers.trace_function("Data timeline multi-graph")
-# def distribution_subplots(data: list, selected_metrics: list, country_labels: list, title: str) -> go.Figure:
-
-#     assert len(data) == len(country_labels)
-#     assert len(selected_metrics) > 0
-#     N = len(country_labels)
-
-#     fig = make_subplots(
-#         rows=len(country_labels),
-#         cols=1,
-#         subplot_titles=country_labels,
-#         shared_xaxes=True,
-#         vertical_spacing=0.05,
-#     )
-
-#     date_rep_counter = {}
-#     for d in data:
-#         for i in d:
-#             date_rep_counter[i["dateRep"]] = date_rep_counter.get(i["dateRep"], 0) + 1
-
-#     date_rep_common = [k for k, v in date_rep_counter.items() if v == N]
-
-#     for n in range(0, N):
-
-#         for s in selected_metrics:
-#             fig.add_trace(
-#                 go.Scatter(
-#                     x=[i["dateRep"] for i in data[n] if i["dateRep"] in date_rep_common],
-#                     y=[
-#                         i[s]
-#                         for i in data[n]
-#                         if i["dateRep"] in date_rep_common
-#                     ],
-#                     mode=MODE_LINES_N_MARKERS,
-#                     name=f"{country_labels[n]} {s.replace('_',' ')}",
-#                 ),
-#                 row=n + 1,
-#                 col=1,
-#             )
-#     fig.update_layout(height=1200, width=1000, title_text=title)
-#     return fig
-
 
 @helpers.trace_function("Case distribution multi-graph")
 def case_distribution_subplots(df: pd.DataFrame, title: str) -> go.Figure:
@@ -102,15 +23,21 @@ def case_distribution_subplots(df: pd.DataFrame, title: str) -> go.Figure:
         vertical_spacing=0.05,
     )
     
-    summary = df.groupby(ecdc.DATE_REP).filter(lambda g: len(g)==N).reset_index()
+    def sort_func(row):
+        yw = row[ecdc.YEAR_WEEK].split('-W')
+        week = yw[1]
+        return int(week)
 
     for n in range(0, N):
         c = countries[n]
-        sub_df = summary.loc[summary[ecdc.COUNTRY] == c].reset_index()
+        sub_df = df.loc[df[ecdc.COUNTRY] == c]
+
+        seq_col = sub_df.apply(sort_func, axis=1)
+        sub_df = sub_df.assign(seq=seq_col.values).sort_values('seq')
 
         fig.add_trace(
             go.Scatter(
-                x=sub_df[ecdc.DATE_REP],
+                x=sub_df[ecdc.YEAR_WEEK],
                 y=sub_df[ecdc.HOSPITAL_RATE],
                 mode=MODE_LINES_N_MARKERS,
                 name=f'{c} {ecdc.HOSPITAL_RATE.replace("_", " ")}',
@@ -121,7 +48,7 @@ def case_distribution_subplots(df: pd.DataFrame, title: str) -> go.Figure:
 
         fig.add_trace(
             go.Scatter(
-                x=sub_df[ecdc.DATE_REP],
+                x=sub_df[ecdc.YEAR_WEEK],
                 y=sub_df[ecdc.DEATHS],
                 mode=MODE_LINES_N_MARKERS,
                 name=f'{c} {ecdc.DEATHS.replace("_", " ")}',
@@ -132,7 +59,7 @@ def case_distribution_subplots(df: pd.DataFrame, title: str) -> go.Figure:
         
         fig.add_trace(
             go.Bar(
-                x=sub_df[ecdc.DATE_REP],
+                x=sub_df[ecdc.YEAR_WEEK],
                 y=sub_df[ecdc.CASES],
                 name=f'{c} {ecdc.CASES.replace("_", " ")}',
             ),
