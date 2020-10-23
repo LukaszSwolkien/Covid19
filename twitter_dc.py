98eded011304e28f247a994b5de295efd5efbbea
lukasz <lukasz.swolkien@gmail.com>
2020-10-23 19:12:18 +0200

    feat: working on otel metrics based on # of tweets for covid

--- twitter_dc.py
+++ twitter_dc.py
@@ -1,4 +1,29 @@
 #! /usr/bin/python
+QUERY_STRING = "#koronawiruspolska"
+FREQUENCY_SEC = 600
+
+from opentelemetry import metrics
+from opentelemetry.sdk.metrics import Counter, MeterProvider
+from opentelemetry.sdk.metrics.export import ConsoleMetricsExporter
+from opentelemetry.sdk.metrics.export.controller import PushController
+
+metrics.set_meter_provider(MeterProvider())
+meter = metrics.get_meter(__name__, True)
+exporter = ConsoleMetricsExporter()
+controller = PushController(meter, exporter, 5)
+
+staging_labels = {"environment": "staging"}
+
+requests_counter = meter.create_metric(
+    name="requests",
+    description=f"{QUERY_STRING} tweets",
+    unit="1",
+    value_type=int,
+    metric_type=Counter,
+    # label_keys=("environment",),
+)
+
+
 import twitter as tw
 from config import settings
 from Poland.tweets_mz import total_cases_and_deaths
@@ -7,7 +32,6 @@ import time
 from dateutil.parser import parse
 from dateutil.tz import tzutc
 
-FREQUENCY_SEC = 60
 
 if __name__ == "__main__":
     t = tw.Twitter(
@@ -26,13 +50,14 @@ if __name__ == "__main__":
     df.to_csv(file_name, index = False, header=True)
 
     while True:
-        covid_tweets = t.search.tweets(q="#koronawiruspolska")
+        covid_tweets = t.search.tweets(q=QUERY_STRING)
         s = covid_tweets.get('statuses', [])
 
         now =  datetime.now(tzutc())
         x_ago = now - timedelta(seconds=FREQUENCY_SEC)
         selected = [item for item in s if parse(item['created_at'])>x_ago]
-        print(len(selected))
+        no_selected = len(selected)
+        requests_counter.add(no_selected, staging_labels)
         for tweet in selected:
             print(f"[{tweet['user']['name']}]{tweet['text']}")
         time.sleep(FREQUENCY_SEC)
